dc6c54ed5cac62670cb76c7b7930ed8ce6927146
xanathon <castillo@web.de>
2019-01-09 17:09:43 +0100

    Update theme.php

--- bluebasic/php/theme.php
+++ bluebasic/php/theme.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *   * Name: Bluebasic
+ *   * Name: BluebasicXan
  *   * Description: This is a variation of the Hubzilla standard theme "Redbasic". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme. 
  *   * Version: Theme Version 20181204<br/><br/>
  *   * MinVersion: 2.3.1
